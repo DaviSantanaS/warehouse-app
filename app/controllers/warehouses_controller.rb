4e94248d6781d57d18970c32091716c865036315
Davi Santana <davisantana@id.uff.br>
2024-04-09 22:47:16 -0300

    adicionando a função de cadastrar um galpao com formulario

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -4,4 +4,23 @@ class WarehousesController < ApplicationController
     @warehouse = Warehouse.find(params[:id])
   end
 
+  def new
+    # @warehouse = Warehouse.new
+  end
+
+
+  def create
+    @warehouse = Warehouse.create(warehouse_params)
+    if @warehouse.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :address, :city, :state, :cep,
+                                      :area, :description, :code)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  resources :warehouses,  only: [:show]
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :warehouses,  only: [:show, :new, :create]
 end
