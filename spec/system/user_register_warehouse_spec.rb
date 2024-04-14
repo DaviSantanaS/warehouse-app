a1fe9633e67dc9d9a5af6385b3940d0b7ebb4288
Davi Santana <davisantana@id.uff.br>
2024-04-13 22:43:21 -0300

    criando arquivos da i18n e padronizando para pt-BR

--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -52,17 +52,28 @@ describe 'Usuário cadastra um galpão' do
         visit root_path
         click_on 'Cadastrar Galpão'
 
-        fill_in 'Nome', with: 'Nome do galpão'
-        fill_in 'Descrição', with: 'Descrição do galpão'
-        fill_in 'Código', with: '1234'
-        fill_in 'Endereço', with: 'Endereço do galpão'
-        fill_in 'Cidade', with: 'Cidade do galpão'
+        fill_in 'Nome', with: ''
+        fill_in 'Descrição', with: ''
+        fill_in 'Código', with: ''
+        fill_in 'Endereço', with: ''
+        fill_in 'Cidade', with: ''
         fill_in 'CEP', with: ''
         fill_in 'Área', with: ''
         click_on 'Cadastrar'
         
         #assert
         expect(page).to have_content('Erro ao cadastrar galpão!')
+        expect(page).to have_content('Nome não pode ficar em branco')
+        expect(page).to have_content('Descrição não pode ficar em branco')
+        expect(page).to have_content('Código não pode ficar em branco')
+        expect(page).to have_content('Endereço não pode ficar em branco')
+        expect(page).to have_content('Cidade não pode ficar em branco')
+        expect(page).to have_content('CEP não pode ficar em branco')
+        expect(page).to have_content('Área não pode ficar em branco')
+        expect(page).to have_content('Código não possui o tamanho esperado (3 caracteres)')
+
+
+
 
     end
 end
