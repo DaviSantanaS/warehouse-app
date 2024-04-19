47760be2429a039742aec97a2236dfd37f1379c7
Davi Santana <davisantana@id.uff.br>
2024-04-18 23:47:17 -0300

    completando modelo de produtos com suas views

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -4,4 +4,29 @@ class ProductModelsController < ApplicationController
     @product_models = ProductModel.all    
   end
 
+  def new
+    @product_model = ProductModel.new
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @product_model = ProductModel.create(product_model_params)
+    if @product_model.save
+      flash[:notice] = 'Modelo de produto cadastrado com sucesso'
+      redirect_to @product_model
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product_model = ProductModel.find(params[:id])
+  end
+
+  private
+  def product_model_params
+    params.require(:product_model).permit(:name, :weight, :height, :width, :length, :sku, :supplier_id)
+  end
+
+
 end
---
+++ spec/system/product_models/user_register_product_model_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'User register product model' do
+  it 'successfully' do
+    Supplier.create!(
+      brand_name: 'Samsung',
+      corporate_name: 'Samsung Eletronics ltda',
+      registration_number: '1234567890',
+      full_address: 'Rua das Flores, 123, São Paulo, SP',
+      city: 'São Paulo',
+      state: 'SP',
+      email: 'samsung@samsung.com',
+      )
+
+    visit root_path
+    click_on 'Modelos de Produtos'
+    click_on 'Cadastrar modelo de produto'
+
+    fill_in 'Nome', with: 'TV 32'
+    fill_in 'Peso', with: 10_000
+    fill_in 'Altura', with: 70
+    fill_in 'Largura', with: 50
+    fill_in 'Comprimento', with: 30
+    fill_in 'SKU', with: 'TV32-SAMS'
+    select 'Samsung', from: 'Fornecedor'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Modelo de produto cadastrado com sucesso')
+    expect(page).to have_content('TV 32')
+    expect(page).to have_content('Peso: 10000g')
+    expect(page).to have_content('Dimensões: 70cm x 50cm x 30cm')
+    expect(page).to have_content('SKU: TV32-SAMS')
+    expect(page).to have_content('Fornecedor: Samsung')
+
+    end
+
+
+
+end
